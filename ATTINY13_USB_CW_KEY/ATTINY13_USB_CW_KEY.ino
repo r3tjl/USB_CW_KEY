22c63df52c0f74feeba280544b324e1c23feeb2a
vAgafonov <r3tjl@mail.ru>
2023-12-26 00:13:49 +0300

    Сделана память знака

--- ATTINY13_USB_CW_KEY/ATTINY13_USB_CW_KEY.ino
+++ ATTINY13_USB_CW_KEY/ATTINY13_USB_CW_KEY.ino
@@ -6,8 +6,8 @@
 // Определяем контакт, к которому подключен потенциометр регулировки скорости электронного ключа
 #define CW_SPEED_PIN A3
 // Определяем максимальную и минимальную длительность точки в миллисекундах
-#define MIN_DIT_TIME 50
-#define MAX_DIT_TIME 150
+#define MIN_DIT_TIME 60     // 50
+#define MAX_DIT_TIME 150    // 150
 
 // Определяем контакты для подключения электронного ключа
 #define CW_DIT_PIN PB2
@@ -19,8 +19,8 @@
 
 // переменные для работы
 
-int t_delay;
-unsigned long timing; // Переменная для хранения точки отсчета millis
+int t_delay, t_delay_dash;
+// unsigned long timing; // Переменная для хранения точки отсчета millis
 int dit_on = 0;
 int dash_on = 0;  // переменные обозначающие нажатую точку или тире
 
@@ -50,46 +50,55 @@ void loop() {
 // обрабатываем нажатие манипулятора "точка"
   if(digitalRead(CW_DIT_PIN) == 0 || dit_on == 1) {
     // расчитываем длительность "точки" по положению потенциометра
-    t_delay = analogRead(CW_SPEED_PIN)*(MAX_DIT_TIME - MIN_DIT_TIME)/1023 + MIN_DIT_TIME;
+    t_delay = analogRead(CW_SPEED_PIN)*(MAX_DIT_TIME - MIN_DIT_TIME)/1023 + MIN_DIT_TIME;  // 1023
 //    t_delay = analogRead(CW_SPEED_PIN)/10 + MIN_DIT_TIME;
     // нажимаем "ключ"      
     digitalWrite(CW_OUT_PIN, HIGH);
+    if (digitalRead(CW_DASH_PIN) == 0) dash_on = 1;
     // включаем звуковой тон
-    analogWrite(CW_SOUND_PIN, 128);
+    analogWrite(CW_SOUND_PIN, 150);
+    if (digitalRead(CW_DASH_PIN) == 0) dash_on = 1;
     // задержка длительности звука/нажатия ключа   
-    if (digitalread(CW_DASH_PIN) == 0) dash_on = 1;   
+    if (digitalRead(CW_DASH_PIN) == 0) dash_on = 1;   
     delay(t_delay);
     // выключаем звуковой тон      
     analogWrite(CW_SOUND_PIN, 0);
+    if (digitalRead(CW_DASH_PIN) == 0) dash_on = 1;
     // "отпускаем" ключ
     digitalWrite(CW_OUT_PIN, LOW);
-    if (digitalread(CW_DASH_PIN) == 0) dash_on = 1;
+    if (digitalRead(CW_DASH_PIN) == 0) dash_on = 1;
     dit_on = 0;
     // выдерживаем паузу между точками/тире      
     delay(t_delay);
+    if (digitalRead(CW_DASH_PIN) == 0) dash_on = 1;
   }
 
 //   обрабатываем нажатие манипулятора "тире"
   if(digitalRead(CW_DASH_PIN) == 0 || dash_on == 1) {
     // расчитываем длительность "точки" по положению потенциометра
     t_delay = analogRead(CW_SPEED_PIN)*(MAX_DIT_TIME - MIN_DIT_TIME)/1023 + MIN_DIT_TIME;
+    t_delay_dash = t_delay * 3.5;
 //    t_delay = analogRead(CW_SPEED_PIN)/10 + MIN_DIT_TIME;
     // нажимаем "ключ"      
     digitalWrite(CW_OUT_PIN, HIGH);
+    if (digitalRead(CW_DIT_PIN) == 0) dit_on = 1;
     // включаем звуковой тон
-    analogWrite(CW_SOUND_PIN, 128);
+    analogWrite(CW_SOUND_PIN, 150);
+    if (digitalRead(CW_DIT_PIN) == 0) dit_on = 1;
     // задержка длительности звука/нажатия ключа      
     // так как у нас тут "тире", то длительность его в 3 раза больше, чем для точки
-    if (digitalread(CW_DIT_PIN) == 0) dit_on = 1;  
-    delay(t_delay*3);
+    if (digitalRead(CW_DIT_PIN) == 0) dit_on = 1;  
+    delay(t_delay_dash);
     // выключаем звуковой тон      
     analogWrite(CW_SOUND_PIN, 0);
+    if (digitalRead(CW_DIT_PIN) == 0) dit_on = 1;
     // "отпускаем" ключ
     digitalWrite(CW_OUT_PIN, LOW);
-    if (digitalread(CW_DIT_PIN) == 0) dit_on = 1;  
+    if (digitalRead(CW_DIT_PIN) == 0) dit_on = 1;  
     dash_on = 0;
     // выдерживаем паузу между точками/тире      
     delay(t_delay);
+    if (digitalRead(CW_DIT_PIN) == 0) dit_on = 1;
   }
 // конец основного цикла
 }
